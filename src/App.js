7fc89d772b353c57e77ca39a7be1de2e54a8e4d7
Yevhen Tkach <150246856+jimmy2jk@users.noreply.github.com>
2024-03-14 21:54:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deployed by Yevhen Tkach
         </p>
         <a
           className="App-link"
